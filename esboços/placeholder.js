dfe70d6db68bee328e54336d733bcaab75f660b1
Razue <lucas.viana.rs@hotmail.com>
2015-11-11 14:57:39 -0200

    Placeholder js

---
+++ esboços/placeholder.js
@@ -0,0 +1,27 @@
+function addPessoa(geracao){
+	
+
+	/*
+	botao = document.getElementById("add-geracao")
+	newgen = document.createElement("div")
+	newgen.setAttribute("class", "geracao")
+
+	id = parseInt(botao.parentElement.id)
+	newgen.setAttribute("id", id + 1)
+
+	botaoPessoa = document.createElement("button")
+	botaoPessoa.setAttribute("onclick", "addPessoa")
+	botaoPessoa.setAttribute("id", "add-pessoa" + (id + 1))
+	botaoPessoa.textContent = "+ Pessoa"
+
+	newgen.appendChild(botaoPessoa)
+	newgen.appendChild(botao)
+
+	hr = document.createElement("hr")
+	root.appendChild(hr)
+	root.appendChild(newgen)
+	*/
+	    
+
+
+}
