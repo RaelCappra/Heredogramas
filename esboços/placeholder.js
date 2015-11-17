fd2dc4ae4bf91dfe98e1467d9a83835ace49b788
Razue <lucas.viana.rs@hotmail.com>
2015-11-17 00:11:07 -0200

    Consertado o bug de duas pessoas serem selecionadas

--- esboços/placeholder.js
+++ esboços/placeholder.js
@@ -8,7 +8,7 @@ function addPessoa(geracao){
 	newgen.setAttribute("title", "Genotipo")
 	//id="draggable"
 	//class="ui-widget-content"
-	newgen.setAttribute("id", "id_pessoa")
+	newgen.setAttribute("id", "id_pessoa"+geracao)
 	//$('#'+openaddress)
 	newgen.setAttribute("class", "ui-widget-content")
 	newgen.addEventListener('build', function () {alert("") })
@@ -55,41 +55,57 @@ function addPessoa(geracao){
 }
 
 function menuPessoa(){
-	var divMenu = document.getElementById("menuPessoa");
-	divMenu.innerHTML = "";
-	var opcoesGene = ["Placeholder 1", "Placeholder 2"];
-	var opcoesSexo = ["Masculino", "Feminino"];
 	
-	var selectAlelo1 = document.createElement("select");
-	selectAlelo1.id = "alelo1";
-	var selectAlelo2 = document.createElement("select");
-	selectAlelo2.id = "alelo2";
 
-	divMenu.appendChild(selectAlelo1);
-	divMenu.appendChild(selectAlelo2);
 
-	for (var i = 0; i < opcoesGene.length; i++) {
-	    var option1 = document.createElement("option");
-	    option1.value = i + 1;
-	    option1.text = opcoesGene[i];
 
-	    var option2 = document.createElement("option");
-	    option2.value = i + 1;
-	    option2.text = opcoesGene[i];
 
-	    selectAlelo1.appendChild(option1);
-	    selectAlelo2.appendChild(option2);
-	}
+  	if($('.ui-selected').length > 1) {
+  		$("p:last").removeClass("intro").addClass("main");
 
-	var selectSexo = document.createElement("select");
-	selectSexo.id = "sexo";
-	divMenu.appendChild(selectSexo);
+	  	$('.ui-selected').each(function () {
+	  		$("#"+$(this).attr("id")).removeClass("ui-selected");
+			
+		});
 
-	for (var i = 0; i < opcoesSexo.length; i++) {
-	    var option = document.createElement("option");
-	    option.value = i + 1;
-	    option.text = opcoesSexo[i];
-	    selectSexo.appendChild(option);
+	}else{
+    
+		var divMenu = document.getElementById("menuPessoa");
+		divMenu.innerHTML = "";
+		var opcoesGene = ["Placeholder 1", "Placeholder 2"];
+		var opcoesSexo = ["Masculino", "Feminino"];
+		
+		var selectAlelo1 = document.createElement("select");
+		selectAlelo1.id = "alelo1";
+		var selectAlelo2 = document.createElement("select");
+		selectAlelo2.id = "alelo2";
+
+		divMenu.appendChild(selectAlelo1);
+		divMenu.appendChild(selectAlelo2);
+
+		for (var i = 0; i < opcoesGene.length; i++) {
+		    var option1 = document.createElement("option");
+		    option1.value = i + 1;
+		    option1.text = opcoesGene[i];
+
+		    var option2 = document.createElement("option");
+		    option2.value = i + 1;
+		    option2.text = opcoesGene[i];
+
+		    selectAlelo1.appendChild(option1);
+		    selectAlelo2.appendChild(option2);
+		}
+
+		var selectSexo = document.createElement("select");
+		selectSexo.id = "sexo";
+		divMenu.appendChild(selectSexo);
+
+		for (var i = 0; i < opcoesSexo.length; i++) {
+		    var option = document.createElement("option");
+		    option.value = i + 1;
+		    option.text = opcoesSexo[i];
+		    selectSexo.appendChild(option);
+		}
 	}
 
 }
