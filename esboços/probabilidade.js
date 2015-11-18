e8d295a203f319e012948e495790d3492556e5a9
Razue <lucas.viana.rs@hotmail.com>
2015-11-18 15:35:52 -0200

    Probabilidade gene autossomico

---
+++ esboços/probabilidade.js
@@ -0,0 +1,45 @@
+
+function calcProbabilidade(probando, gene){
+	var list = [];  
+	var possibilidades = [];
+	if(gene.getAttribute("cromossomo") == "X" || get.getAttribute("cromossomo") == "Y"){
+		//crossing over default com o genero envolvido
+		//combinações = geneAPai/geneAMae, geneAPai/geneBMae, geneBPAI/geneAMae, geneBPai/geneBMae
+		
+	}else{
+		list.push(probando.pai.alelo1+"/"+probando.mae.alelo1)
+		list.push(probando.pai.alelo2+"/"+probando.mae.alelo1)
+		list.push(probando.pai.alelo1+"/"+probando.mae.alelo2)
+		list.push(probando.pai.alelo2+"/"+probando.mae.alelo2)
+
+		for (var i = 0; i < list.length; i++) {
+			countInArray(list, list[i]);
+		};
+		
+		var count = 0;
+		for (var i = 0; i < possibilidades.length; i++) {
+			count++;
+
+		};
+
+		for (var i = 0; i < possibilidades.length; i++) {
+
+			possibilidades[i] = possibilidades[i]/ possibilidades.length;
+
+		};
+		return possibilidades;
+
+		//pego um da lista -> conta 1 possibilidades -> removo ele da lista
+	}
+}
+
+function countInArray(list, fenotipo) {
+    var count = 0;
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] === fenotipo) {
+            count++;
+            delete array[i];
+        }
+    }
+    possibilidades.push(count);
+}
