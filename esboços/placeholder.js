c6d1767cf596402b9e194a377d3985dd3fea0e3b
RaelCappra <rael.cappra@gmail.com>
2015-11-18 05:54:42 -0200

    --BROKEN ALERT-- começando a integrar os eventos de mundança do menu com a pessoa no model propriamente dito

--- esboços/placeholder.js
+++ esboços/placeholder.js
@@ -24,7 +24,8 @@ function addPessoa(geracao){
             probando: false,
             sexo: 0,
             alelo1: 0,
-            alelo2: 0
+            alelo2: 0,
+            id: lastIdPessoa
         }
         pessoas[lastIdPessoa] = pessoa
         
@@ -67,77 +68,97 @@ function addPessoa(geracao){
 
 }
 
+//return: se algum menu foi de fato fechado
+function closeMenu(){
+    if($('.ui-selected').length > 1) {
+        $(".ui-selected").removeClass("ui-selected");
+    }   
+    return true
+}
+
 function menuPessoa(pessoa){
-	
-	var divMenu = document.getElementById("menuPessoa");
-		divMenu.innerHTML = "";
-
-
-  	if($('.ui-selected').length > 1) {
-  		$(".ui-selected").removeClass("ui-selected");
-
-
-	}else{
+    console.log("id: " + pessoa.id)
     
-
-		
-		var selectAlelo1 = document.createElement("select");
-		selectAlelo1.id = "alelo1";
-		var selectAlelo2 = document.createElement("select");
-		selectAlelo2.id = "alelo2";
-
-		divMenu.appendChild(selectAlelo1);
-		divMenu.appendChild(selectAlelo2);
-
-		for (var i = 0; i < opcoesGene.length; i++) {
-		    var option1 = document.createElement("option");
-		    option1.value = i + 1;
-		    option1.text = opcoesGene[i];
-
-		    var option2 = document.createElement("option");
-		    option2.value = i + 1;
-		    option2.text = opcoesGene[i];
-
-		    selectAlelo1.appendChild(option1);
-		    selectAlelo2.appendChild(option2);
-		}
-
-		var selectSexo = document.createElement("select");
-		selectSexo.id = "sexo";
-		divMenu.appendChild(selectSexo);
-
-		for (var i = 0; i < opcoesSexo.length; i++) {
-		    var option = document.createElement("option");
-		    option.value = i;
-                    if (i == pessoa.sexo){
-                        option.setAttribute("selected", "true")
-                    }
-		    option.text = opcoesSexo[i];
-		    selectSexo.appendChild(option);
-		}
-
-		var selectAnalise = document.createElement("select");
-		selectAnalise.id = "selectAnalise";
-		divMenu.appendChild(selectAnalise);
-
-		var opcoesAnalise = ["Probando", "Normal"];
-		for (var i = 0; i < opcoesAnalise.length; i++) {
-		    var option = document.createElement("option");
-		    option.value = i;
-		    option.text = opcoesAnalise[i];
-                    if (pessoa.probando){
-                        if (i == 0)
-                            option.setAttribute("selected", "true")
-                    }
-                    else if (i == 1){
-                        option.setAttribute("selected", "true")
-                    }
-
-		    selectAnalise.appendChild(option);
-		    
-		}
+    var divMenu = document.getElementById("menuPessoa");
+        divMenu.innerHTML = "";
 
 
-	}
+    if (!closeMenu()){
+        var selectAlelo1 = document.createElement("select");
+        selectAlelo1.id = "alelo1";
+        var selectAlelo2 = document.createElement("select");
+        selectAlelo2.id = "alelo2";
+
+        divMenu.appendChild(selectAlelo1);
+        divMenu.appendChild(selectAlelo2);
+
+        for (var i = 0; i < opcoesGene.length; i++) {
+            var option1 = document.createElement("option");
+            option1.value = i + 1;
+            option1.text = opcoesGene[i];
+
+            var option2 = document.createElement("option");
+            option2.value = i + 1;
+            option2.text = opcoesGene[i];
+
+            selectAlelo1.appendChild(option1);
+            selectAlelo2.appendChild(option2);
+        }
+
+        //definindo a combobox do sexo
+        var selectSexo = document.createElement("select");
+        selectSexo.id = "sexo";
+        divMenu.appendChild(selectSexo);
+        
+        console.log("sexo: " + pessoa.sexo)
+
+        for (var i = 0; i < opcoesSexo.length; i++) {
+            var option = document.createElement("option");
+            option.value = i;
+            if (i == pessoa.sexo){
+                option.setAttribute("selected", "true")
+            }
+            option.text = opcoesSexo[i];
+            selectSexo.appendChild(option);
+        }
+
+        $(selectSexo).change(function(){
+            pessoa.sexo = selectSexo[selectSexo.selectedIndex].value
+        })
+        
+
+        //definindo a combobox da analise (Probando/Normal)
+        var selectAnalise = document.createElement("select");
+        selectAnalise.id = "selectAnalise";
+        divMenu.appendChild(selectAnalise);
+
+        var opcoesAnalise = {true: "Probando", false:"Normal"};
+        optionTrue = document.createElement("option");
+        optionFalse = document.createElement("option");
+
+        optionTrue.value = true;
+        optionTrue.text = opcoesAnalise[true];
+
+        optionFalse.value = false;
+        optionFalse.text = opcoesAnalise[false];
+        console.log("probando: " + pessoa.probando)
+        if (pessoa.probando){
+            optionTrue.setAttribute("selected", "true")
+        }
+        else{
+            optionFalse.setAttribute("selected", "true")
+        }
+
+        selectAnalise.appendChild(optionTrue);
+        selectAnalise.appendChild(optionFalse);
+            
+        //alteracao no selectAnalise -> alteracao na pessoa (modelo)
+        $(selectAnalise).change(function(){
+            pessoa.probando = selectAnalise[selectAnalise.selectedIndex].value === 'true'
+        })
+    }
+
 
 }
+
+
